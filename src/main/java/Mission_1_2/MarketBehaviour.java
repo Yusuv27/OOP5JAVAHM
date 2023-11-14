e58e1b1995af2fd56a9ef5e859a813965dfb729e
Юсув <kingysuwkillerspy@mail.ru>
2023-11-14 22:10:23 +0300

    Revert "Seminar_2)"
    This reverts commit e587388d36ab7e0ac4906bab49580eb80e05ee90.

--- src/main/java/Mission_1_2/MarketBehaviour.java
+++
@@ -1,9 +0,0 @@
-package Mission_1_2;
-
-import java.util.List;
-
-public interface MarketBehaviour {
-    void acceptToMarket(Actor actor);
-    void releaseFromMarket(List<Actor> actors);
-    void update(List<Actor> actors);
-}
